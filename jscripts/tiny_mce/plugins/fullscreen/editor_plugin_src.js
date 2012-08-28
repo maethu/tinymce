6ce2ed13f6f44507216e64178323b79b14511cf4
Tom Gross <itconsense@gmail.com>
2012-08-28 15:39:37 +0200

    fix timeout bug occuring in chrome (see discussion in 91143d72a3)

--- jscripts/tiny_mce/plugins/fullscreen/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/fullscreen/editor_plugin_src.js
@@ -25,16 +25,16 @@
 					if (ed.getParam('fullscreen_new_window'))
 						closeFullscreen(); // Call to close in new window
 					else {
+						tinymce.dom.Event.remove(DOM.win, 'resize', t.resizeFunc);
+						tinyMCE.get(ed.getParam('fullscreen_editor_id')).setContent(ed.getContent());
 						DOM.win.setTimeout(function() {
-							tinymce.dom.Event.remove(DOM.win, 'resize', t.resizeFunc);
-							tinyMCE.get(ed.getParam('fullscreen_editor_id')).setContent(ed.getContent());
 							tinyMCE.remove(ed);
 							DOM.remove('mce_fullscreen_container');
 							de.style.overflow = ed.getParam('fullscreen_html_overflow');
 							DOM.setStyle(DOM.doc.body, 'overflow', ed.getParam('fullscreen_overflow'));
 							DOM.win.scrollTo(ed.getParam('fullscreen_scrollx'), ed.getParam('fullscreen_scrolly'));
 							tinyMCE.settings = tinyMCE.oldSettings; // Restore old settings
-						}, 10);
+						}, 100);
 					}
 
 					return;
